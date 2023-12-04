783e84161a70676d8c8145d73e8abc171ff7f34d
onikesum <egg0427@naver.com>
2023-12-04 17:53:56 +0900

    Update MusicController, musicService, musicServiceImpl

---
+++ src/main/java/com/example/springplaylist/MusicService.java
@@ -0,0 +1,10 @@
+package com.example.springplaylist;
+
+import java.util.ArrayList;
+
+public interface MusicService {
+    public ArrayList<MusicDto> findAll();
+    public MusicDto findById(int id);
+    public void delete(int idx);
+    public MusicDto save(MusicDto music);
+}
