aebde91c3bd50abd1382983a6d4c7d8e39cb199e
milkioiy <93613157+milkioiy@users.noreply.github.com>
2023-12-04 12:06:47 +0900

    initial config

---
+++ src/main/java/com/example/springplaylist/SpringplaylistApplication.java
@@ -0,0 +1,13 @@
+package com.example.springplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringplaylistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringplaylistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springplaylist/SpringplaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringplaylistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
